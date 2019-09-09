cc458d82a16a336aff605482dcb07abb8a3eed5a
pubxun <pubxun@126.com>
2019-09-09 12:34:29 +0800

    自动打开浏览器

--- pma-desktop-win/pma-desktop.bat
+++ pma-desktop-win/pma-desktop.bat
@@ -1,1 +1,5 @@
+for /f "tokens=3,4" %%a in ('"reg query HKEY_CLASSES_ROOT\http\shell\open\command"') do (set SoftWareRoot=%%a %%b)
+start "" % SoftWareRoot % "http://localhost:88/"
+
 %TEMP%/pma-desktop-win/php.exe -S localhost:88 -t %TEMP%/pma-desktop-win/
+
